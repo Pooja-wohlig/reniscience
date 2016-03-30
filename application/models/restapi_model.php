4e98d22480fab6e354a4b7b02c1084d98104fead
Pooja <pthakare33@gmail.com>
2016-03-30 11:41:33 +0530

    api modified

--- application/models/restapi_model.php
+++ application/models/restapi_model.php
@@ -4,12 +4,12 @@ exit( "No direct script access allowed" );
 class restapi_model extends CI_Model
 {
 public function getstories($storyid){
-$query['story']=$this->db->query("SELECT `id`,`title`,`content`,`numberofimage`,`image1`,`image2`,`status` FROM `reniscience_story` WHERE `id`='$storyid'")->result();
+$query['story']=$this->db->query("SELECT `id`,`title`,`content`,`numberofimage`,`image1`,`image2`,`status`,`visibility` FROM `reniscience_story` WHERE `id`='$storyid'")->result();
 	$query['storyimage']=$this->db->query("SELECT `reniscience_storyimage`.`id`,`reniscience_storyimage`.`storyid`,`reniscience_storyimage`.`order`,`reniscience_storyimage`.`image`,`reniscience_storyimage`.`status`,`reniscience_story`.`title` FROM `reniscience_story` LEFT OUTER JOIN `reniscience_storyimage` ON `reniscience_storyimage`.`storyid`=`reniscience_story`.`id` WHERE `reniscience_story`.`id`='$storyid'")->result();
 		return $query;
 }
 	public function getallstories(){
-	$query=$this->db->query("SELECT `id`,`title`,`content`,`numberofimage`,`image1`,`image2`,`status` FROM `reniscience_story`")->result();
+	$query=$this->db->query("SELECT `id`,`title`,`content`,`numberofimage`,`image1`,`image2`,`status`,`visibility` FROM `reniscience_story`")->result();
 		
 		 foreach($query AS $row)
         {
